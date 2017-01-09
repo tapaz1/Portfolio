d782c9c4f9a172434100cdfd327d4721c7b69ab3
Adam Paz <timothy.paz@us.sogeti.com>
2017-01-09 16:43:18 -0600

    Adding sogeti SVG logo and animation as well as profile container. Also added Angular Material 2 and SnapSvg.js

--- Portfolio-App/src/app/app.component.ts
+++ Portfolio-App/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Adam Paz Portfolio coming soon..";
+
+  public contentIsLoaded = false;
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Load profile');
+      this.contentIsLoaded = true;
+    }, 5000)
+  }
+
+  title = "Adam Paz Portfolio coming..";
 }
